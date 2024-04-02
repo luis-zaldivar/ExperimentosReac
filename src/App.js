8e499db71efb0efdd76cc7f526179a64398831e9
luis-zaldivar <lalozaldivarbarza@gmail.com>
2024-04-02 16:15:59 -0600

    uso de tarjetas

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './componentes/login/Login.js';
+import './CSS/StiloLog.css'
+
+const App = () => {
+  return (
+    <div className='tarjeta-contenedo'>
+      <Login />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/componentes/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className='tarjeta'>
+      <h1>Título del componente secundario</h1>
+      <p>Contenido del componente secundario</p>
+    </div>
+  );
+};
+
+export default Login;
